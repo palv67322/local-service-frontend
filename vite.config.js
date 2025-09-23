9c4953677b24ab18ecedf52e05f6ff6d0965ab66
unknown <palv67322@gmail.com>
2025-09-23 22:49:08 +0530

    Initial commit — local-service-frontend

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//vishalpal
 export default defineConfig({
   plugins: [
     tailwindcss(),
